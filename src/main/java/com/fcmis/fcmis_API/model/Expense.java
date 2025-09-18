0954904fb65d031713a147646b95c44a29e9a831
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-09-18 14:53:07 +0600

    Expense an Sale model created

---
+++ src/main/java/com/fcmis/fcmis_API/model/Expense.java
@@ -0,0 +1,20 @@
+package com.fcmis.fcmis_API.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Expense {
+    private enum Type{Advertising, Delivery, OTHER};
+    private long id;
+    private Type type;
+    private LocalDate date;
+    private BigDecimal amount;
+    private String note;
+}
---
+++ src/main/java/com/fcmis/fcmis_API/model/Sale.java
@@ -0,0 +1,24 @@
+package com.fcmis.fcmis_API.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sale {
+    private long id;
+    private  Product product;
+    private LocalDate date;
+    private  Integer quantity;
+    private BigDecimal unitSellPrice;
+    private BigDecimal totalAmount;
+    private BigDecimal unitBuyPrice;
+    private BigDecimal cogAmount;
+    
+
+}

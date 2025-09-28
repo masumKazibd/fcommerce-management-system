1da47b87c2749ce037ea455d25eb4487ee5f2a80
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-09-28 23:54:42 +0600

    update main

--- src/main/java/com/fcmis/fcmis_API/FcmisApiApplication.java
+++ src/main/java/com/fcmis/fcmis_API/FcmisApiApplication.java
@@ -2,8 +2,12 @@ package com.fcmis.fcmis_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.fcmis.fcmis_API.domain")
+@EnableJpaRepositories("com.fcmis.fcmis_API.repo")
 public class FcmisApiApplication {
 
 	public static void main(String[] args) {
